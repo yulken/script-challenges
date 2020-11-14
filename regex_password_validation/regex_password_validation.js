245b623d98c9e877dbc0e484389ab29609b6950b
Caio Torres <caiocftorres@gmail.com>
2020-11-14 17:59:58 -0300

    adding regex validation readme file

--- regex_password_validation/regex_password_validation.js
+++ regex_password_validation/regex_password_validation.js
@@ -1,11 +1,1 @@
-// https://www.codewars.com/kata/52e1476c8147a7547a000811/train/javascript
-// You need to write regex that will validate a password to make sure it meets the following criteria:
-
-//     At least six characters long
-//     contains a lowercase letter
-//     contains an uppercase letter
-//     contains a number
-
-// Valid passwords will only be alphanumeric characters.
-
 const validate = (password => /^(?!.*[;\.])(?=.*[a-z])(?=.*[A-Z])(?=.*\d)\w{6,}/.test(password))
